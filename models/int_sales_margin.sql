5f5bf51e770157ee862fb43f25cf365c127e7f82
dimitrinsili97@gmail.com <dimitrinsili97@gmail.com>
2025-02-12 11:57:51 +0000

    sales margin

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,12 +1,12 @@
 SELECT
-      products_id,
-      date_date,
-      orders_id,
-      revenue,
-      quantity,
+      s.product_id,
+      s.date_date,
+      s.orders_id,
+      s.revenue,
+      s.quantity,
       purchase_price,
       ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
       ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
-  FROM {{ref("stg_raw__sales")}} s
+  FROM {{ ref('stg_raw__sales') }} s
   LEFT JOIN {{ref("stg_raw__product")}} p
-    ON s.pdt_id = p.products_id
+    ON s.product_id = p.products_id
